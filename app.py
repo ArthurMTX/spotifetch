07cc530d37fc8364ad3eb56c944be91ae7325d2b
ArthurMTX <itsmitralyx@gmail.com>
2024-05-06 17:20:16 +0200

    Started UI

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
 import requests
 import time
-
+from flask import Flask, jsonify
 from data_processing import calculate_stats
 from spotify_api import get_access_token, PLAYLIST_API_URL, get_all_tracks
+from flask import render_template
+import plotly.graph_objects as go
+import plotly.offline as po
 
 app = Flask(__name__)
 
@@ -44,9 +46,53 @@ def get_playlist_info(playlist_id):
                         'request_time_seconds': round(time.time() - start_time, 2)}), response.status_code
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/playlist/<playlist_id>/charts', methods=['GET'])
+def get_playlist_charts(playlist_id):
+    response = get_playlist_info(playlist_id)
+    result = response.get_json()
+
+    # Infos générales
+    fig_info = go.Figure(data=[go.Table(
+        header=dict(values=['Name', 'Description', 'Followers', 'Owner'],
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[result['name'], result['description'], result['followers'], result['owner']],
+                   fill_color='lavender',
+                   align='left'))
+    ])
+    fig_info_html = po.plot(fig_info, output_type='div', include_plotlyjs=False)
+
+    # Top 10 artistes
+    artists = result['stats']['artists_sorted_by_appearance'][:20]
+    fig_artists = go.Figure(data=[go.Bar(
+        x=[artist[0] for artist in artists],
+        y=[artist[1] for artist in artists],
+        text=[artist[1] for artist in artists],
+        textposition='auto',
+    )])
+    fig_artists_html = po.plot(fig_artists, output_type='div', include_plotlyjs=False)
+
+    # Top 10 labels
+    labels = result['stats']['labels_sorted_by_appearance'][:20]
+    fig_labels = go.Figure(data=[go.Bar(
+        x=[label[0] for label in labels],
+        y=[label[1] for label in labels],
+        text=[label[1] for label in labels],
+        textposition='auto',
+    )])
+    fig_labels_html = po.plot(fig_labels, output_type='div', include_plotlyjs=False)
+
+    # Top 10 genres
+    genres = result['stats']['genres_sorted_by_appearance'][:20]
+    fig_genres = go.Figure(data=[go.Bar(
+        x=[genre[0] for genre in genres],
+        y=[genre[1] for genre in genres],
+        text=[genre[1] for genre in genres],
+        textposition='auto',
+    )])
+    fig_genres_html = po.plot(fig_genres, output_type='div', include_plotlyjs=False)
+
+    return render_template('charts.html', fig_info_html=fig_info_html, fig_artists_html=fig_artists_html, fig_labels_html=fig_labels_html, fig_genres_html=fig_genres_html)
 
 
 if __name__ == '__main__':
